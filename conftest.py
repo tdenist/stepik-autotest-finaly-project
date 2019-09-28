a6df2b7ad2dcde860c97e420098f89ce235ab0cb
Denis Tohomirov <dent.90@mail.ru>
2019-09-28 11:57:18 +0300

    добавлена проверка наличия ссылки на страницу логина

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en', help="Input language (default ru)")
