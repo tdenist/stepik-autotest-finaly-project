ef9ae7807e52b6b51cafe8ed56659084db1115a3
Denis Tohomirov <dent.90@mail.ru>
2019-09-28 11:33:43 +0300

    исправлен файл теста главной страницы в стиле Page Object, исправлены ошибки в классе базовой страницы

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,5 +2,5 @@ class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-	def open(self):
-	    self.browser.get(self.url)
+    def open(self):
+        self.browser.get(self.url)
